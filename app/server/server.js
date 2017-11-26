93c4e9a28880316d173f876c92a5ba6e41139f7b
lilawren <li.lawrly@gmail.com>
2017-11-26 00:03:28 -0800

    setup api encapsulation, basic api test

--- app/server/server.js
+++ app/server/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 var express = require('express')
 var path = require('path')
+var request = require('request');
 
 var DIST_DIR = path.join(__dirname, '../client/build');
 var PORT = 8000;
@@ -14,9 +17,35 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 })
 
+
+const RIOT_API_KEY = process.env.RIOT_API_KEY;
+
+// RIOT api
+// /api/user?name=lali
+app.get('/api/user', function (req, res) {
+    const name = req.query.name;
+    if (name == null || name.length == 0) {
+        res.end();
+        return;
+    }
+
+    var apiReq = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + name + '?api_key=' + RIOT_API_KEY;
+
+    request(apiReq, function (error, response, body) {
+        if (error) {
+            console.log('error:', error);
+            res.end();
+            return;
+        }
+        console.log('statusCode:', response && response.statusCode);
+        res.send(body)
+    });
+})
+
 var server = app.listen(PORT, function (error) {
     if (error) {
         console.log(error);
     }
     console.log('Listening on port ' + PORT);
 })
+
